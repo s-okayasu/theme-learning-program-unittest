d6aac3e47f9007f794c296100f9936b3c2e095bb
takaoka <cig108196@gmail.com>
2026-08-25 21:02:34 +0900

    8/25勉強会　テストケース追加

--- java_project/src/test/java/com/example/grade/GradeReportTest.java
+++ java_project/src/test/java/com/example/grade/GradeReportTest.java
@@ -5,14 +5,21 @@ import com.example.grade.AbstractGradeReport;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GradeReportTest {
+    String className;
+    int totalClassCount;
+    String studentName;
+    @BeforeEach
+    public void setUp() {
+        className = "数学";
+        totalClassCount = 5;
+        studentName = "山田太郎";
+    }
+
     @Test
-    void testJudgeGrade() { 
+    void testGredeReport_A() { 
     
         try{
             // インプット情報を定義
-            String className = "数学";
-            int totalClassCount = 5;
-            String studentName = "山田太郎";
             int examScore = 80;
             int attendanceCount = 5;
 
@@ -24,6 +31,67 @@ class GradeReportTest {
             e.printStackTrace();
         }    
     }
+
+    @Test
+    void testGredeReport_B() { 
+    
+        try{
+            // インプット情報を定義
+            String className = "数学";
+            int totalClassCount = 5;
+            String studentName = "山田太郎";
+            int examScore = 60;
+            int attendanceCount = 4;
+
+            // 成績判定を呼び出し
+            GradeReport gradeReports = new GradeReport(className, totalClassCount);
+            String grade = gradeReports.judgeGrade(studentName, examScore, attendanceCount);
+            assertEquals("数学授業の山田太郎さんの評価はBです。",grade); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }    
+    }
+
+    @Test
+    void testGredeReport_C() { 
+    
+        try{
+            // インプット情報を定義
+            String className = "数学";
+            int totalClassCount = 5;
+            String studentName = "山田太郎";
+            int examScore = 40;
+            int attendanceCount = 3;
+
+            // 成績判定を呼び出し
+            GradeReport gradeReports = new GradeReport(className, totalClassCount);
+            String grade = gradeReports.judgeGrade(studentName, examScore, attendanceCount);
+            assertEquals("数学授業の山田太郎さんの評価はCです。",grade); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }    
+    }
+
+    @Test
+    void testGredeReport_D() { 
+    
+        try{
+            // インプット情報を定義
+            String className = "数学";
+            int totalClassCount = 5;
+            String studentName = "山田太郎";
+            int examScore = 39;
+            int attendanceCount = 2;
+
+            // 成績判定を呼び出し
+            GradeReport gradeReports = new GradeReport(className, totalClassCount);
+            String grade = gradeReports.judgeGrade(studentName, examScore, attendanceCount);
+            assertEquals("数学授業の山田太郎さんの評価はDです。",grade); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }    
+    }
+
 
     @Test
     void testValidateInput_True() { 
